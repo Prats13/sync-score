a3d3dd81d425c0f088430f42ed5f2a6e61d8f1e5
pratyush patra <xpratyush@freo.money>
2026-04-09 00:44:35 +0530

    sync-score/issues/#1 - v0.0.0 ``` - code cleanup ```
    Signed-off-by: pratyush patra <xpratyush@freo.money>

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.proof_artifact import ProofArtifact
-from app.models.capability import CapabilityDeclaration, CapabilityDocument
-
-__all__ = ["User", "ProofArtifact", "CapabilityDeclaration", "CapabilityDocument"]
